61a6d0eaf1be5edfd430b9e0793ff4d95eca0344
Victor Dorneanu <info@dornea.nu>
2021-12-15 08:56:24 +0100

    Elfeed changes

--- dot_doom.d/+elfeed.el
+++ dot_doom.d/+elfeed.el
@@ -13,135 +13,154 @@
   ;;       :config
   ;;       (elfeed-goodies/setup))
 
+  (defun elfeed-search-format-date (date)
+    (format-time-string "%Y-%m-%d %H:%M" (seconds-to-time date)))
+
   (setq elfeed-search-filter "@1-week-ago +unread"
-        elfeed-search-print-entry-function '+rss/elfeed-search-print-entry
-        elfeed-search-title-min-width 180
-        elfeed-show-entry-switch #'pop-to-buffer
-        elfeed-show-entry-delete #'+rss/delete-pane
-        elfeed-show-refresh-function #'+rss/elfeed-show-refresh--better-style
+        elfeed-search-print-entry-function 'elfeed-search-print-entry--default
+        elfeed-search-title-min-width 600
+        elfeed-show-entry-switch 'elfeed-display-buffer
+        ;; elfeed-show-entry-delete #'+rss/delete-pane
+        ;; elfeed-show-refresh-function #'+rss/elfeed-show-refresh--better-style
         shr-max-image-proportion 0.6)
 
   (add-hook! 'elfeed-show-mode-hook (hide-mode-line-mode 1))
   (add-hook! 'elfeed-search-update-hook #'hide-mode-line-mode)
 
-  (defface elfeed-show-title-face '((t (:weight ultrabold :slant italic :height 1.5)))
-    "title face in elfeed show buffer"
-    :group 'elfeed)
-  (defface elfeed-show-author-face `((t (:weight light)))
-    "title face in elfeed show buffer"
-    :group 'elfeed)
-  (set-face-attribute 'elfeed-search-title-face nil
-                      :foreground 'nil
-                      :weight 'light)
+  (defun elfeed-display-buffer (buf &optional act)
+    (pop-to-buffer buf)
+    (set-window-text-height (get-buffer-window) (round (* 0.95 (frame-height)))))
 
-  (defadvice! +rss-elfeed-wrap-h-nicer ()
-    "Enhances an elfeed entry's readability by wrapping it to a width of
-`fill-column' and centering it with `visual-fill-column-mode'."
-    :override #'+rss-elfeed-wrap-h
-    (setq-local truncate-lines nil
-                shr-width 120
-                visual-fill-column-center-text t
-                default-text-properties '(line-height 1.1))
-    (let ((inhibit-read-only t)
-          (inhibit-modification-hooks t))
-      (visual-fill-column-mode)
-      ;; (setq-local shr-current-font '(:family "Merriweather" :height 1.2))
-      (set-buffer-modified-p nil)))
-
-  (defun +rss/elfeed-search-print-entry (entry)
-    "Print ENTRY to the buffer."
-    (let* ((elfeed-goodies/tag-column-width 30)
-           (elfeed-goodies/feed-source-column-width 30)
-           (title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
-           (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
-           (feed (elfeed-entry-feed entry))
-           (feed-title
-            (when feed
-              (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
-           (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
-           (tags-str (concat (mapconcat 'identity tags ",")))
-           (title-width (- (window-width) elfeed-goodies/feed-source-column-width
-                           elfeed-goodies/tag-column-width 4))
-
-           (tag-column (elfeed-format-column
-                        tags-str (elfeed-clamp (length tags-str)
-                                               elfeed-goodies/tag-column-width
-                                               elfeed-goodies/tag-column-width)
-                        :left))
-           (feed-column (elfeed-format-column
-                         feed-title (elfeed-clamp elfeed-goodies/feed-source-column-width
-                                                  elfeed-goodies/feed-source-column-width
-                                                  elfeed-goodies/feed-source-column-width)
-                         :left)))
-
-      (insert (propertize feed-column 'face 'elfeed-search-feed-face) " ")
-      (insert (propertize tag-column 'face 'elfeed-search-tag-face) " ")
-      (insert (propertize title 'face title-faces 'kbd-help title))
-      (setq-local line-spacing 0.2)))
-
-  (defun +rss/elfeed-show-refresh--better-style ()
-    "Update the buffer to match the selected entry, using a mail-style."
+  (defun my/elfeed-show-eww (&optional link)
     (interactive)
-    (let* ((inhibit-read-only t)
-           (title (elfeed-entry-title elfeed-show-entry))
-           (date (seconds-to-time (elfeed-entry-date elfeed-show-entry)))
-           (author (elfeed-meta elfeed-show-entry :author))
-           (link (elfeed-entry-link elfeed-show-entry))
-           (tags (elfeed-entry-tags elfeed-show-entry))
-           (tagsstr (mapconcat #'symbol-name tags ", "))
-           (nicedate (format-time-string "%a, %e %b %Y %T %Z" date))
-           (content (elfeed-deref (elfeed-entry-content elfeed-show-entry)))
-           (type (elfeed-entry-content-type elfeed-show-entry))
-           (feed (elfeed-entry-feed elfeed-show-entry))
-           (feed-title (elfeed-feed-title feed))
-           (base (and feed (elfeed-compute-base (elfeed-feed-url feed)))))
-      (erase-buffer)
-      (insert "\n")
-      (insert (format "%s\n\n" (propertize title 'face 'elfeed-show-title-face)))
-      (insert (format "%s\t" (propertize feed-title 'face 'elfeed-search-feed-face)))
-      (when (and author elfeed-show-entry-author)
-        (insert (format "%s\n" (propertize author 'face 'elfeed-show-author-face))))
-      (insert (format "%s\n\n" (propertize nicedate 'face 'elfeed-log-date-face)))
-      (when tags
-        (insert (format "%s\n"
-                        (propertize tagsstr 'face 'elfeed-search-tag-face))))
-      ;; (insert (propertize "Link: " 'face 'message-header-name))
-      ;; (elfeed-insert-link link link)
-      ;; (insert "\n")
-      (cl-loop for enclosure in (elfeed-entry-enclosures elfeed-show-entry)
-               do (insert (propertize "Enclosure: " 'face 'message-header-name))
-               do (elfeed-insert-link (car enclosure))
-               do (insert "\n"))
-      (insert "\n")
-      (if content
-          (if (eq type 'html)
-              (elfeed-insert-html content base)
-            (insert content))
-        (insert (propertize "(empty)\n" 'face 'italic)))
-      (goto-char (point-min))))
-  )
+    (let* ((entry (if (eq major-mode 'elfeed-show-mode)
+                      elfeed-show-entry
+                    (elfeed-search-selected :ignore-region)))
+           (link (if link link (elfeed-entry-link entry))))
+      (eww link)
+      (add-hook 'eww-after-render-hook 'eww-readable nil t)))
+  (define-key elfeed-search-mode-map (kbd "e") 'my/elfeed-show-eww)
+  (define-key elfeed-show-mode-map (kbd "e") 'my/elfeed-show-eww)
 
-;; from https://www.reddit.com/r/emacs/comments/g6oowz/elfeed_rules/
-(defun ap/elfeed-search-browse-org ()
-  "Open selected items as Org."
-  (interactive)
-  (let ((browse-url-browser-function (lambda (url _)
-                                       (org-web-tools-read-url-as-org url))))
-    (ap/elfeed-search-selected-map #'ap/elfeed-search-browse-entry)))
+;;   (defface elfeed-show-title-face '((t (:weight ultrabold :slant italic :height 1.5)))
+;;     "title face in elfeed show buffer"
+;;     :group 'elfeed)
+;;   (defface elfeed-show-author-face `((t (:weight light)))
+;;     "title face in elfeed show buffer"
+;;     :group 'elfeed)
+;;   (set-face-attribute 'elfeed-search-title-face nil
+;;                       :foreground 'nil
+;;                       :weight 'light)
 
-(defun ap/elfeed-search-browse-entry (entry)
-  "Browse ENTRY with `browse-url' and mark as read.
-If ENTRY is unread, it will also be unstarred.  To override the
-browser function, bind `browse-url-browser-function' around the
-call to this."
-  (let ((url (elfeed-entry-link entry))
-        (tags (elfeed-entry-tags entry)))
-    ;; Mark as read first, because apparently the elfeed functions don't work after `browse-url'
-    ;; potentially changes the buffer.
-    (elfeed-untag entry 'unread)
-    (elfeed-search-update-entry entry)
-    (browse-url url)))
+;;   (defadvice! +rss-elfeed-wrap-h-nicer ()
+;;     "Enhances an elfeed entry's readability by wrapping it to a width of
+;; `fill-column' and centering it with `visual-fill-column-mode'."
+;;     :override #'+rss-elfeed-wrap-h
+;;     (setq-local truncate-lines nil
+;;                 shr-width 120
+;;                 visual-fill-column-center-text t
+;;                 default-text-properties '(line-height 1.1))
+;;     (let ((inhibit-read-only t)
+;;           (inhibit-modification-hooks t))
+;;       (visual-fill-column-mode)
+;;       ;; (setq-local shr-current-font '(:family "Merriweather" :height 1.2))
+;;       (set-buffer-modified-p nil)))
 
-(cl-defun ap/elfeed-search-selected-map (fn)
-  "Map FN across selected entries in elfeed-search buffer using `mapcar'."
-  (mapcar fn (elfeed-search-selected)))
+;;   (defun +rss/elfeed-search-print-entry (entry)
+;;     "Print ENTRY to the buffer."
+;;     (let* ((elfeed-goodies/tag-column-width 30)
+;;            (elfeed-goodies/feed-source-column-width 30)
+;;            (title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
+;;            (title-faces (elfeed-search--faces (elfeed-entry-tags entry)))
+;;            (feed (elfeed-entry-feed entry))
+;;            (feed-title
+;;             (when feed
+;;               (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
+;;            (tags (mapcar #'symbol-name (elfeed-entry-tags entry)))
+;;            (tags-str (concat (mapconcat 'identity tags ",")))
+;;            (title-width (- (window-width) elfeed-goodies/feed-source-column-width
+;;                            elfeed-goodies/tag-column-width 4))
+
+;;            (tag-column (elfeed-format-column
+;;                         tags-str (elfeed-clamp (length tags-str)
+;;                                                elfeed-goodies/tag-column-width
+;;                                                elfeed-goodies/tag-column-width)
+;;                         :left))
+;;            (feed-column (elfeed-format-column
+;;                          feed-title (elfeed-clamp elfeed-goodies/feed-source-column-width
+;;                                                   elfeed-goodies/feed-source-column-width
+;;                                                   elfeed-goodies/feed-source-column-width)
+;;                          :left)))
+
+;;       (insert (propertize feed-column 'face 'elfeed-search-feed-face) " ")
+;;       (insert (propertize tag-column 'face 'elfeed-search-tag-face) " ")
+;;       (insert (propertize title 'face title-faces 'kbd-help title))
+;;       (setq-local line-spacing 0.2)))
+
+;;   (defun +rss/elfeed-show-refresh--better-style ()
+;;     "Update the buffer to match the selected entry, using a mail-style."
+;;     (interactive)
+;;     (let* ((inhibit-read-only t)
+;;            (title (elfeed-entry-title elfeed-show-entry))
+;;            (date (seconds-to-time (elfeed-entry-date elfeed-show-entry)))
+;;            (author (elfeed-meta elfeed-show-entry :author))
+;;            (link (elfeed-entry-link elfeed-show-entry))
+;;            (tags (elfeed-entry-tags elfeed-show-entry))
+;;            (tagsstr (mapconcat #'symbol-name tags ", "))
+;;            (nicedate (format-time-string "%a, %e %b %Y %T %Z" date))
+;;            (content (elfeed-deref (elfeed-entry-content elfeed-show-entry)))
+;;            (type (elfeed-entry-content-type elfeed-show-entry))
+;;            (feed (elfeed-entry-feed elfeed-show-entry))
+;;            (feed-title (elfeed-feed-title feed))
+;;            (base (and feed (elfeed-compute-base (elfeed-feed-url feed)))))
+;;       (erase-buffer)
+;;       (insert "\n")
+;;       (insert (format "%s\n\n" (propertize title 'face 'elfeed-show-title-face)))
+;;       (insert (format "%s\t" (propertize feed-title 'face 'elfeed-search-feed-face)))
+;;       (when (and author elfeed-show-entry-author)
+;;         (insert (format "%s\n" (propertize author 'face 'elfeed-show-author-face))))
+;;       (insert (format "%s\n\n" (propertize nicedate 'face 'elfeed-log-date-face)))
+;;       (when tags
+;;         (insert (format "%s\n"
+;;                         (propertize tagsstr 'face 'elfeed-search-tag-face))))
+;;       ;; (insert (propertize "Link: " 'face 'message-header-name))
+;;       ;; (elfeed-insert-link link link)
+;;       ;; (insert "\n")
+;;       (cl-loop for enclosure in (elfeed-entry-enclosures elfeed-show-entry)
+;;                do (insert (propertize "Enclosure: " 'face 'message-header-name))
+;;                do (elfeed-insert-link (car enclosure))
+;;                do (insert "\n"))
+;;       (insert "\n")
+;;       (if content
+;;           (if (eq type 'html)
+;;               (elfeed-insert-html content base)
+;;             (insert content))
+;;         (insert (propertize "(empty)\n" 'face 'italic)))
+;;       (goto-char (point-min))))
+;;   )
+
+;; ;; from https://www.reddit.com/r/emacs/comments/g6oowz/elfeed_rules/
+;; (defun ap/elfeed-search-browse-org ()
+;;   "Open selected items as Org."
+;;   (interactive)
+;;   (let ((browse-url-browser-function (lambda (url _)
+;;                                        (org-web-tools-read-url-as-org url))))
+;;     (ap/elfeed-search-selected-map #'ap/elfeed-search-browse-entry)))
+
+;; (defun ap/elfeed-search-browse-entry (entry)
+;;   "Browse ENTRY with `browse-url' and mark as read.
+;; If ENTRY is unread, it will also be unstarred.  To override the
+;; browser function, bind `browse-url-browser-function' around the
+;; call to this."
+;;   (let ((url (elfeed-entry-link entry))
+;;         (tags (elfeed-entry-tags entry)))
+;;     ;; Mark as read first, because apparently the elfeed functions don't work after `browse-url'
+;;     ;; potentially changes the buffer.
+;;     (elfeed-untag entry 'unread)
+;;     (elfeed-search-update-entry entry)
+;;     (browse-url url)))
+
+;; (cl-defun ap/elfeed-search-selected-map (fn)
+;;   "Map FN across selected entries in elfeed-search buffer using `mapcar'."
+;;   (mapcar fn (elfeed-search-selected)))
+)
