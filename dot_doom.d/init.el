f9c13dce1c7cb105963ca2b0306798c18bce6546
Victor Dorneanu <info@dornea.nu>
2023-08-24 15:43:56 +0200

    Content change

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -24,8 +24,9 @@
        (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +childframe)               ; a search engine for love and life
-       ;; vertico           ; the search engine of the future
+       ;; (ivy)
+                                        ; a search engine for love and life
+       (vertico +icons)           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -188,7 +189,7 @@
        emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
+       (rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
