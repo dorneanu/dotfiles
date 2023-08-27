1d12e564df2d0c49aaba051fb291362119c2efd5
Victor Dorneanu <info@dornea.nu>
2023-08-27 22:02:20 +0200

    Working on mu4e

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -97,7 +97,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
+       (lookup +offline +dictionary)              ; navigate your code and its documentation
        lsp               ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
