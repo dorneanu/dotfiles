326308d4e4181e4a784ce0e5ba4c88e59ebf689d
Victor Dorneanu <info@dornea.nu>
2023-11-13 10:12:08 +0100

    Content change

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -37,9 +37,9 @@
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures   ; ligatures and symbols to make your code pretty again
+       ;; ligatures   ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       modeline    ; snazzy, Atom-inspired modeline, plus API
+       modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints                ; highlight the region an operation acts on
@@ -47,7 +47,7 @@
        ;; tabs              ; a tab bar for Emacs
        treemacs               ; a project drawer, like neotree but cooler
        unicode                ; extended unicode support for various languages
-       (vc-gutter +pretty)    ; vcs diff in the fringe
+       ;; (vc-gutter +pretty)    ; vcs diff in the fringe
        vi-tilde-fringe        ; fringe tildes to mark beyond EOB
        window-select          ; visually switch windows
        workspaces             ; tab emulation, persistence & separate workspaces
