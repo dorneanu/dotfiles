9078d498c5e42f2ad15efa73f8b558d9f3503a5f
Victor Dorneanu <info@dornea.nu>
2024-01-20 14:10:31 +0100

    Update Go version

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -8,7 +8,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 TARGET="/tmp/$(id -u)-go.tar.gz"
 
 ansi --green "Downloading Go binary"
-wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz -O $TARGET
+wget https://golang.org/dl/go1.21.0.linux-amd64.tar.gz -O $TARGET
 
 ansi --green "Installing Go"
 tar -C $HOME/.local/ -xvzf $TARGET
