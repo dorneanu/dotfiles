a462786b2e61a08d5aa37e56f0a2a4fc2a25e407
Victor Dorneanu <info@dornea.nu>
2024-01-30 18:17:25 +0100

    Auto-tangle files

---
+++ emacs/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-confirm-babel-evaluate . nil)
+              (eval .
+                    (progn
+                      ;; Tangle whole file after being saved
+                      (add-hook 'after-save-hook 'org-babel-tangle)
+                      )))))
